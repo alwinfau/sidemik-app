08a37c4ea890b9624092d5be4ea4ec38ea920c81
tabita <tabitaanggi27@gmail.com>
2025-04-08 14:58:56 +0700

    update

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -14,6 +14,7 @@ import { Link, usePage } from '@inertiajs/react';
 import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-react';
 import AppLogo from './app-logo';
 import AppLogoIcon from './app-logo-icon';
+import { ThemeProvider } from './theme-provider';
 
 const mainNavItems: NavItem[] = [
     {
